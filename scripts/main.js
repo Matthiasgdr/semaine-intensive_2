2a7c741cf16ac41656e66883f4b68050825891ef
RomainMaillot <32742569+RomainMaillot@users.noreply.github.com>
2018-02-15 13:15:55 +0100

    rien

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let flechegauche = document.getElementsByClassName('flechedroite')[0]
+let flechedroite = document.getElementsByClassName('flechegauche')[0]
+let imgs = document.getElementsByClassName('w-100')
+let index = 0
+
+function goUp() {
+  console.log('up');
+  if (index < imgs.length*100-100) {
+    index += 100
+    for (var i = 0; i < imgs.length; i++) {
+      imgs[i].style.transform=`translateY(-${index}%)`
+    }
+  }
+}
+
+function goDown() {
+  console.log('down');
+  if (index > 0) {
+    index -= 100
+    for (var i = 0; i < imgs.length; i++) {
+      imgs[i].style.transform=`translateY(-${index}%)`
+    }
+  }
+}
+
+flechedroite.addEventListener('click', () => {
+      goUp()
+})
+
+flechegauche.addEventListener('click', () => {
+      goDown()
+})
+
+setInterval(function () {
+  if (index < imgs.length*100-100)
+  {
+    goUp()
+  }else {
+    goDown()
+    index -= 200
+  }
+}, 4000);
